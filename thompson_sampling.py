37052d0959f150cf7782070c88f82c9dbb2657a7
sonyashijin <65740491+sonyashijin@users.noreply.github.com>
2023-12-05 13:48:59 -0800

    fixed syntax error

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -18,5 +18,5 @@ class DirichletMultinomialThompsonSampling:
         self.alpha[chosen_arm] += reward
     def reset(self):
     # Reset alpha to uniform prior
-        logging.debug("Resetting alpha values to uniform prior.")v
+        logging.debug("Resetting alpha values to uniform prior.")
         self.alpha = np.ones(self.num_arms)
