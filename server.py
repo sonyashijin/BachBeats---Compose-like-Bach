2f0d782e197fa0b86fa39b2362614e776742e7f9
sonyashijin <65740491+sonyashijin@users.noreply.github.com>
2023-12-06 11:17:23 -0800

    changed reward calc

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def load_and_train_model():
 # load and train data at start up
 load_and_train_model()
 
-def calculate_reward(initial_score, updated_score, max_change=1.5, weight_factor=10):
+def calculate_reward(initial_score, updated_score, max_change= 0.9, weight_factor=20):
     # calculate the difference in score
     score_change = updated_score - initial_score
 
